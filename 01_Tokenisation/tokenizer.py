4f25a6847a95c61710bc30a58985bd63ecff5ba8
Varun Sreedhar <62417466+VarunS9000@users.noreply.github.com>
2022-11-11 01:47:01 +0530

    segmenter and  tokenizer in python

---
+++ 01_Tokenisation/tokenizer.py
@@ -0,0 +1,16 @@
+import sys
+
+line = sys.stdin.readline()
+
+while line!='':
+    
+    line = line.replace('.',' .')
+    line = line.replace(',',' ,')
+    line = line.replace(';',' ;')
+    line = line.replace(':',' :')
+    line = line.replace(' ','\n')
+
+    sys.stdout.write(line)
+    line = sys.stdin.readline()
+
+#Punctuations are considered seperate tokens
